c6dcb92942a62212573c219784de75acb7f18b45
CARLES135 <carles.wamba@korv-consulting.fr>
2026-02-24 20:29:38 +0100

    working on mission contract

--- custom_addons/mission_contract/__manifest__.py
+++ custom_addons/mission_contract/__manifest__.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'SLA Contracts',
+    'name': 'Mission Contracts',
     'author': 'KORV Consulting',
     'website': 'www.korv-consulting.fr',
     'category': 'Sales',
-    'summary': 'Gestion des contrats SLA liés aux commandes',
+    'summary': 'Gestion contrats Mission liés aux commandes',
     'version': '1.0',
     'depends': ['sale'],
     'data': [
-        'data/sla_cron.xml',
         'security/ir.model.access.csv',
-        'report/sla_contract_report.xml',
-        'views/sla_contract_views.xml',
+        'report/mission_contract_report.xml',
+        'views/mission_contract_views.xml',
         'views/sale_order_views.xml',
     ],
     'installable': True,
